8ca5f69245af4be6c7952ad034fa4447c3abdbac
cjohnson018 <fun2bcd@gmail.com>
2023-09-19 13:41:31 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int cardBalance = 5000;
+        double interest = .17;
+        double monthOneInterest = cardBalance * interest;
+        double monthTwoInterest = monthOneInterest * 1.17;
+        System.out.println("The interest due after one month is " + monthOneInterest);
+        System.out.println("The interest due after two months is " + monthTwoInterest);
+    }
+}
